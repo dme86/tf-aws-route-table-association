7b75a5b31cbe1fc4c6a7fd2d3ef708dc74ce99e5
Dan <me.daniel.meier@gmail.com>
2021-08-07 15:17:49 +0100

    no public output

--- outputs.tf
+++ outputs.tf
@@ -2,8 +2,3 @@ output "private_route_table_association_ids" {
   description = "List of IDs of the private route table association"
   value       = aws_route_table_association.private.*.id
 }
-
-output "public_route_table_association_ids" {
-  description = "List of IDs of the public route table association"
-  value       = aws_route_table_association.public.*.id
-}
